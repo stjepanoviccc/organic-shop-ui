3a2d949b1e5e7396876a97e6907b8486ddbb4a97
Andrej <you@example.com>
2023-08-07 14:02:52 +0200

    folder structure addded, firebase connected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import classes from './App.module.scss';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('https://react-organic-shop-5b019-default-rtdb.firebaseio.com/.json')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className={classes.flow}>
       Hello World
