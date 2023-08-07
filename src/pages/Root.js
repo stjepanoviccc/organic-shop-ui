5c433e4211be66c64bb0f99fc7a115303314d01c
Andrej <you@example.com>
2023-08-08 01:37:34 +0200

    switched cBrowser with cHash  router (gh problem)

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 import NavigationMenu from "../components/NavigationMenu";
 
 const RootLayout = () => {
-    <>
+    return (
+        <>
         <NavigationMenu />
         <main>
             <Outlet />
         </main>
     </>
+    )
 };
 
 export default RootLayout;
