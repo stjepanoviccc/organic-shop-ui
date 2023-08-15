41adbd0f8089cf544b4285b147258a14e0e8bd60
Andrej <you@example.com>
2023-08-16 00:13:04 +0200

    fixed image on card

--- src/components/UI/cards/AdsCard.js
+++ src/components/UI/cards/AdsCard.js
@@ -1,12 +1,17 @@
+import useCheckImagePath from '../../../custom_hooks/CheckImagePath';
 import GreenButton from '../buttons/GreenButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import styles from './AdsCard.module.scss';
 
 const AdsCard = () => {
+    const bgImageUrl = useCheckImagePath(`${process.env.PUBLIC_URL}/static/media/organic-food-card-img.jpg`, './static/media/organic-food-card-img.jpg');
+    const bgImageStyle = {
+        backgroundImage: `url(${bgImageUrl})`,
+      };
 
     return (
-        <div className={styles.adsCard}>
+        <div className={styles.adsCard} style={bgImageStyle}>
             <div className={styles.adsOverlay}></div>
             <div className={styles.adsContent}>
                 <div className={styles.contentHolder}>
