a401645a528d3b925514917c89f731a0e3aff757
Andrej <you@example.com>
2023-09-03 13:54:46 +0200

    started working on form validation

--- src/components/UI/buttons/GreenButton.js
+++ src/components/UI/buttons/GreenButton.js
@@ -1,10 +1,10 @@
 import styles from './GreenButton.module.scss';
 
 const GreenButton = (props) => {
-
+    console.log(props.disabled)
     return (
         <div className={props.class && styles.btnHolder}>
-            <button className={styles.greenBtn} onClick={props.onClick} >
+            <button disabled={props.disabled ? props.disabled : false}className={styles.greenBtn} onClick={props.onClick} >
                 {props.children}
             </button>
         </div>
--- src/components/product-page/ReviewForm.js
+++ src/components/product-page/ReviewForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
 import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
@@ -6,26 +6,68 @@ import GreenButton from '../UI/buttons/GreenButton';
 import styles from './ReviewForm.module.scss';
 
 const ReviewForm = ({ data }) => {
-    const [hoveredStar, setHoveredStar] = useState(null);
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredReview, setEnteredReview] = useState('');
+    const [nameIsValid, setNameIsValid] = useState(false);
+    const [emailIsValid, setEmailIsValid] = useState(false);
+    const [reviewIsValid, setReviewIsValid] = useState(false);
+    const [formIsValid, setFormIsValid] = useState(false);
 
+    const nameChangeHandler = (event) => {
+        const newName = event.target.value
+        setEnteredName(newName);
+        const isValid = (newName.length > 3 && newName.length < 30) && (newName[0] === newName[0].toUpperCase());
+        setNameIsValid(isValid);
+    };
+
+    const emailChangeHandler = (event) => {
+        const newEmail = event.target.value;
+        setEnteredEmail(newEmail);
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValid = emailRegex.test(newEmail);
+        setEmailIsValid(isValid);
+    };
+
+    const reviewChangeHandler = (event) => {
+        const reviewMessage = event.target.value;
+        setEnteredReview(reviewMessage);
+        const isValid = reviewMessage.length > 10 && reviewMessage.length < 100;
+        setReviewIsValid(isValid);
+    };
+
+    useEffect(() => {
+        if (nameIsValid && emailIsValid && reviewIsValid) {
+          setFormIsValid(true);
+        } else {
+          setFormIsValid(false);
+        }
+      }, [nameIsValid, emailIsValid, reviewIsValid]);
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(`${enteredName} + ${nameIsValid}`)
+        console.log(`${enteredEmail} + ${emailIsValid}`)
+        console.log(`${enteredReview} + ${reviewIsValid}`)
+        console.log(formIsValid);
+    }
+
+    // rating star logic
+    const [hoveredStar, setHoveredStar] = useState(null);
     const handleStarHover = (index) => {
         setHoveredStar(index);
     };
-
     const handleStarLeave = () => {
         setHoveredStar(null);
     };
-
     const starIcons = Array.from({ length: 5 }).map((_, index) => (
         <button key={index} className={styles.starIconButton} onMouseEnter={() => handleStarHover(index)} onMouseLeave={handleStarLeave} onClick={() => console.log(index + 1)}>
-            <FontAwesomeIcon
-                className={styles.starIcon}
-                icon={hoveredStar === null ? faRegularStar : index <= hoveredStar ? faSolidStar : faRegularStar} />
+            <FontAwesomeIcon className={styles.starIcon} icon={hoveredStar === null ? faRegularStar : index <= hoveredStar ? faSolidStar : faRegularStar} />
         </button>
     ));
 
     return (
-        <div className={styles.reviewForm}>
+        <form onSubmit={formSubmitHandler} className={styles.reviewForm}>
             <div className={styles.formContentWrap}>
                 <h3 className={styles.formTitle}>
                     {data.reviews === undefined && `Be the first to review "${data.title}"`}
@@ -41,24 +83,24 @@ const ReviewForm = ({ data }) => {
             </div>
             <div className={styles.formContentWrap}>
                 <p className={styles.formText}>Your Review *</p>
-                <textarea className={styles.formTextArea}></textarea>
+                <textarea className={styles.formTextArea} onBlur={reviewChangeHandler}></textarea>
             </div>
             <div className={styles.formNameAndEmailWrap}>
                 <div className={styles.formContentWrap}>
                     <p className={styles.formText}>Name *</p>
-                    <input className={styles.formInput} type="text"></input>
+                    <input className={styles.formInput} type="text" onBlur={nameChangeHandler}></input>
                 </div>
                 <div className={styles.formContentWrap}>
                     <p className={styles.formText}>Email *</p>
-                    <input className={styles.formInput} type="text"></input>
+                    <input className={styles.formInput} type="text" onBlur={emailChangeHandler}></input>
                 </div>
             </div>
             <div className={styles.formCheckboxWrap}>
                 <input type="checkbox"></input>
                 <p>Save my name, email, and website in this browser for the next time I comment.</p>
             </div>
-            <GreenButton class={true}>SUBMIT</GreenButton>
-        </div>
+            <GreenButton disabled={!formIsValid} class={true}>SUBMIT</GreenButton>
+        </form>
     )
 };
 
